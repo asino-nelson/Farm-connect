b7a90928abb71afaa49327245bade898225d682e
asino-nelson <asinonelson276@gmail.com>
2024-02-22 09:15:06 +0300

    login signup form commit

---
+++ login.js
@@ -0,0 +1,41 @@
+ //formOpenBtn = document.querySelector("#form-open"),
+const home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+//formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+function func(){
+  // var email = document.querySelector(".email");
+  // var password = document.querySelector(".password");
+
+  // if (email == !null && password == !null){
+    window.location.assign("index.html")
+  // }
+ 
+}
